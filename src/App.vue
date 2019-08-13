199828d39927ca42708aa35d7101a58d32655dce
liunanchenFYJJ <liunan_chen@163.com>
2019-08-13 18:43:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+html {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    #app {
+      height: 100%;
+    }
+  }
+}
+</style>
